5cdb05a12633b87e08b39709a134ea4db8f9ae1a
nrmliGit <mammadliglnr15@gmail.com>
2024-10-11 04:40:46 -0700

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 300;
+});
+backBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 300;
+});
